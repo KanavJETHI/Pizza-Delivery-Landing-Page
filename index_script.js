dd7bd2a16aa3e9c9a7c2cd07f8abe6a3384a286f
Kanav.Jethi-12215538 <117577182+KanavJETHI@users.noreply.github.com>
2024-07-17 20:58:03 +0530

    Add files via upload

---
+++ index_script.js
@@ -0,0 +1,13 @@
+function validateLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    if (username === 'kanav' && password === 'Kan@1904') {
+        window.location.href = 'hero_page.html';  
+        return false;
+    } else {
+        errorMessage.textContent = 'Invalid username or password';
+        return false;
+    }
+}
